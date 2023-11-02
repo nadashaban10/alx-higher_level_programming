43f9775f24b1633a95ba77f5648da7f90f2ef3e0
nadashaban10 <nadashapann@gmail.com>
2023-11-02 15:32:25 -0700

    dn

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def print_argvs(args):
+  """Prints the number and list of arguments.
+
+  Args:
+    args: A list of arguments.
+  """
+
+  number_of_args = len(args)
+  if number_of_args == 0:
+    print("0 arguments.")
+  else:
+    print(f"{number_of_args} arguments:")
+    for i in range(number_of_args):
+      print(f"{i + 1}: {args[i]}")
+
+if __name__ == "__main__":
+  print_argvs(sys.argv[1:])

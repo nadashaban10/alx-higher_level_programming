b6bfa61987051bdfb7c677b39898db54a2f4fa75
nadashaban10 <nadashapann@gmail.com>
2023-11-02 16:34:29 -0700

    dnnnn

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
+
 def print_argvs(args):
-  """Prints the number and list of arguments.
+    """Prints the number and list of arguments.
 
-  Args:
-    args: A list of arguments.
-  """
+    Args:
+        args: A list of arguments.
+    """
 
-  number_of_args = len(args) - 1
-  print(f"{number_of_args} arguments:")
+    number_of_args = len(args)
+    if number_of_args == 0:
+        print("0 arguments.")
+    elif number_of_args == 1:
+        print(f"1 argument:")
+        print(f"1: {args[0]}")
+    else:
+        print(f"{number_of_args} arguments:")
+        for i in range(number_of_args):
+            print(f"{i + 1}: {args[i]}")
 
-  for i in range(number_of_args):
-    print(f"{i + 1}: {args[i]}")
-
-  if number_of_args == 0:
-    print("0 arguments.")
-  else:
-    if number_of_args == 1:
-      print(f"{number_of_args} argument:")
-      print(f"1: {args[i]}")
+if __name__ == "__main__":
+    print_argvs(sys.argv[1:])

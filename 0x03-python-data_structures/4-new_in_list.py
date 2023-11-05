84aa100fe94cb975bc968752c09e09d2ccabd403
nadashaban10 <nadashapann@gmail.com>
2023-11-05 10:15:43 -0800

    copping

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,6 +6,8 @@ def new_in_list(my_list, idx, element):
     if idx > range_list:
         return my_list.copy()
     else:
-        my_list[idx] = element
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
         return my_list
     

72454725458391eeab16bd61afd619c6848be676
nadashaban10 <nadashapann@gmail.com>
2023-11-05 08:57:17 -0800

    replacment

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    x = len(my_list)
+    if idx < 0:
+        return " none"
+    if idx > x - 1:
+        return "none"
+    else:
+        my_list[idx] = element
+        return (my_list[idx])

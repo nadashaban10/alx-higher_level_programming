ca8ca3c4e2204378d1b1dd40351f77aae84f821a
nadashaban10 <nadashapann@gmail.com>
2023-11-05 01:52:17 -0800

    dn

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    x = len( mylist)
+    if idx < 0:
+        return " none"
+    if idx > x - 1:
+        return "none"
+    else:
+        return (mylist[idx])

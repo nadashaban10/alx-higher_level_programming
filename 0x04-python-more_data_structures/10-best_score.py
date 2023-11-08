ed3a4b38dc6cd96bda5c794d9ba2643f1b625914
nadashaban10 <nadashapann@gmail.com>
2023-11-08 14:34:30 -0800

    dn

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        list_dic = list(a_dictionary.key())
+        max_value = 0
+        leader = ""
+        for i in list_dic:
+            if a_dictionary[i] > max_value:
+                max_value = a_dictionary[i]
+                leader = i
+        return leader

f075492ad8e2e376cc54cf3afe352d95a480c927
nadashaban10 <nadashapann@gmail.com>
2023-11-08 12:28:25 -0800

    delet keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     sorted_dictionary = sorted(a_dictionary.items())
     for key, value in sorted_dictionary:
         print("{}: {}" .format(key, value))
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,5 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
-        return None
     else:
         return a_dictionary

82a27105b71691aeacface35f5fc0df6f4b62c9b
nadashaban10 <nadashapann@gmail.com>
2023-11-13 12:10:18 -0800

    dn101

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for char in chr(ord('a') + 1)[::-1]:
+for char in chr(ord('z') + 1)[::-1]:
     if char.isupper():
         char.lower()
-else:
-    char
-    print("{:s}" .format(char), end='')
+    else:
+         char
+    print("{:s}".format(char), end='')

c3d27faaaf1c343533190324041abb12b44f3f06
nadashaban10 <nadashapann@gmail.com>
2023-11-20 15:01:20 -0800

    dn

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    try:
+
         result = 0
         for i in range(1, 3):
-            if i > a:
-                raise Exception('Too far')
-            result += (a ** b) // i
-    except Exception as e:
-        print("Exception:", e, file=sys.stderr)
-        return None
-    else:
-        return result + b
+            try:
+                if i > a:
+                    raise Exception('Too far')
+                result += (a ** b) // i
+            except Exception:
+                result = a + b
+                break
+            return result

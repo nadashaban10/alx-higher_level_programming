65bc7be1640a2666e4840e7c59f465c239b964db
nadashaban10 <nadashapann@gmail.com>
2023-11-21 14:44:24 -0800

    difine a class that prints a # using __size

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,8 +32,8 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-
         """prints a # using size of"""
+
         if self.__size == 0:
             print("")
         for i in range(self.__size):

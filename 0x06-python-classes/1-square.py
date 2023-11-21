80499189b868ada1ba455de74cdbe96d87bca3ed
nadashaban10 <nadashapann@gmail.com>
2023-11-21 12:04:00 -0800

    square class with priv inistance

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" define a class with a priv atrr """
 class Square:
+    """ represent the square """
+
     def __init__(self, size):
+        """ initialize the Sqaure with size """
+
         self.__size = size

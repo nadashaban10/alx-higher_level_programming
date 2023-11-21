c2184e2f91172c7b0dc6a6a640c3a0dd981bec21
nadashaban10 <nadashapann@gmail.com>
2023-11-21 13:54:28 -0800

    class access using getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 
-"""Define a class """
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
 
-    def __init__(self, size=0):
-        """Initialize a new square."""
-        self.__size = size
+    def init(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """Retrieve the current size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Calculate and return the current area of the square."""
-        return self.__size * self.__size
+        """Return the current area of the square."""
+        return (self.__size * self.__size)

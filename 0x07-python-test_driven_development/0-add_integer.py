98414ac9b1c4298f069eebe75911318ef787760d
nadashaban10 <nadashapann@gmail.com>
2023-11-24 17:02:59 -0800

    tests file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define add_integer function."""
+
+
+def add_integer(a, b=98):
+    """Args: a,b integer or float numbers
+    Return: sumtion of two integer
+    """
+    if (not isinstance(a, int) and (not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and (not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    result = int(a) + int(b)
+    return result
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""this is module that prints my name , with 2 str given"""
+def say_my_name(first_name, last_name=""):
+    """ funtion prints with 2 parameter
+    Args:
+        first_name: fisrt name to print.
+        last_name: last name to print.
+
+    Return:
+        void , full name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}" .format(first_name, last_name))

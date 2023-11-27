2f1247c79d2794957c429e7470e6ccaeedd71450
nadashaban10 <nadashapann@gmail.com>
2023-11-27 14:29:34 -0800

    rectangle class with area and primeter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -38,5 +38,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError(" height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value

5b6a25d2b026068cc2339c9e82c81292f05e7f1b
nadashaban10 <nadashapann@gmail.com>
2023-11-27 16:49:03 -0800

    pycodestyle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-             return ""
+            return ""
 
         rectangle_p = ""
         for i in range(self.height):

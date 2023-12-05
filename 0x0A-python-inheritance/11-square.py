28ba8f70a8f05ce30f512471102511217cb15b85
nadashaban10 <nadashaban10@github.com>
2023-12-05 04:24:36 +0200

    square2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for class that inherits from REctangle
+"""
+
+"""Parent class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Basic class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size must be private. No getter or setter, must be a positive
+            integer, validated by integer_validator
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size

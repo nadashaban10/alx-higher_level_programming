e0615e300623a81991a4fe1a347204eea524674e
nadashaban10 <nadashaban10@github.com>
2023-12-06 03:18:41 +0200

    json

---
+++ 0x0B-python-input_output/ 7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""modules imported"""
+import json
+import sys
+
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+file = "add_item.json"
+try:
+    new = load_file(file)
+except (FileNotFoundError, ValueError):
+    new = []
+    for args in sys.argv[1:]:
+        new.append(args)
+        save_file(new, file)

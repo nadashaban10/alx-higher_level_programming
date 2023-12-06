4ebfa8923d1edcd143b2d038a5a721ab985ec52b
nadashaban10 <nadashaban10@github.com>
2023-12-06 02:34:34 +0200

    JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,23 +3,22 @@
 
 
 def class_to_json(obj):
-  """
-  Returns a dictionary description of an object suitable for JSON serialization.
+    """
+    Returns a dictionary description
 
-  Args:
-    obj: An instance of a class.
+    Args:
+        obj: An instance of a class.
 
-  Returns:
-    A dictionary representing the object's attributes and their JSON-serializable values.
+    Returns:
+        A dictionary representing the obj
 
-  """
-  result = {}
-  for key, value in obj.__dict__.items():
-    if isinstance(value, (list, dict, str, int, bool)):
-      '''Simple data types can be directly included'''
-      result[key] = value
-    else:
-      '''Skip non-serializable values'''
-      pass
-  return result
-
+    """
+    result = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            '''Simple data types can be directly included'''
+            result[key] = value
+        else:
+            '''Skip non-serializable values'''
+            pass
+    return result

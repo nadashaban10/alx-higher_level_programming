a35b572e1ec08690b5c17567c608bb363d3c48b7
nadashaban10 <nadashaban10@github.com>
2023-12-09 02:41:03 +0200

    JSON

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-class Student:
-    """Represents a student."""
+"""class module"""
 
+
+class Student:
+    """Student class"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student."""
+        """initialized method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the student."""
-        if not attrs:
-            return self.__dict__
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        """retrievers a dictionary representation of student instance.
+
+
+        Args:
+            attrs: attributes
+
+        Return: dictionary.
+        """
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()

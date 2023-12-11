45e58fde50160b5262a678cf95e7b370934c2c4a
nadashaban10 <nadashaban10@github.com>
2023-12-11 20:32:36 +0200

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,7 +17,7 @@ class Square(Rectangle):
             y: The Y of the square.
             id: ID of the square.
         '''
-        self.size = size
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -61,13 +61,11 @@ class Square(Rectangle):
                     raise TypeError("id must be an integer")
                 self.id = args[0]
             if len(args) > 1:
-                self.width = args[1]
+                self.size = args[1]
             if len(args) > 2:
-                self.height = args[2]
+                self.x = args[2]
             if len(args) > 3:
-                self.x = args[3]
-            if len(args) > 4:
-                self.y = args[4]
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 if key == "id":

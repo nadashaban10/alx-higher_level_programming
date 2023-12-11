2daffe565ce7779fac593d9143b1d0c4d90916e0
nadashaban10 <nadashaban10@github.com>
2023-12-11 20:17:51 +0200

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,8 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    '''repressention square'''
+
     def __init__(self, size, x=0, y=0, id=None):
         '''
         Initialize a new square object.

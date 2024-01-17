47b4db7dc6b617a46203fc0ca676e4cfd9a068c2
nadashaban10 <nadashaban10@github.com>
2024-01-17 04:23:38 +0200

    done16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- number of same score
+SELECT score, COUNT(*) AS number 
+FROM second_table 
+GROUP BY score 
+ORDER BY score DESC;
+
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- list all values with name
+SELECT score, name FROM second_table ORDER BY score DESC; 

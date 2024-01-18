cd9df41659d3ad923ba61cfd97d4b7134c1452b5
nadashaban10 <nadashaban10@github.com>
2024-01-18 03:55:35 +0200

    done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
--- craete cities in the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Create a table wit FOREING KEY
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 use hbtn_0d_usa;
-CREATE TABLE cities IF NOT EXISTS (
-	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cities`
+(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 	state_id INT NOT NULL,
-	name VARCHAR(256) AND IS NOT NULL,
+	name VARCHAR(256),
 	FOREIGN KEY(state_id) REFERENCES states(id)
-	);
-
+);

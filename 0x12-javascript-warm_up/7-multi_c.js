f6685c8a499115846421a870cb7944758d07dfd9
nadashaban10 <nadashaban10@github.com>
2024-02-12 21:13:55 +0200

    javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ function CustomFun () {
     console.log('Missing number of occurrences');
   } else {
     for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
+      console.log('C is fun');
+    }
   }
 }
 CustomFun();

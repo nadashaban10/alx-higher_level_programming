33533d76b0d36349445ee665931ba66b6d5945f0
nadashaban10 <nadashaban10@github.com>
2024-02-13 16:32:38 +0200

    objects

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  function CustomOne () {
+    if (w, h < 0 || w, h === 0)
+    return {}
+  }
+  module.exports = Rectangle;
+  

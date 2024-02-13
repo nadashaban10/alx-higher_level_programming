9fa8ea269bdc093ed913463e04db410134b79781
nadashaban10 <nadashaban10@github.com>
2024-02-13 02:41:11 +0200

    javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-function PrintSqu () {
-    const x = parseInt(process.argv[2], 10);
-    if (isNaN(x) || x === undefined) {
-      console.log('Missing size');
-    } else {
-      for (let i = 0; i < x; i++) {
-        const squaree = x * x;
-        console.log(`X * ${squaree}`);
-      }
-    }
+
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
   }
-  PrintSqu();
-  
+}

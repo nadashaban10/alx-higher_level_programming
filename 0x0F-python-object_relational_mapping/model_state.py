377e550998e897080d1494fdffe502ce2f9035d3
nadashaban10 <nadashaban10@github.com>
2024-02-16 23:14:24 +0200

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-    State class and Base, an instance of declarative_base()
-"""
-from sqlalchemy import Column, Integer, String, MetaData
+"""a python file that contains the class definition of a State
+and an instance Base = declarative_base()"""
+
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+
+Base = declarative_base()
 
 
 class State(Base):
-    """
-    Class with id and name attr
-    """
-    tablename = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    """State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)

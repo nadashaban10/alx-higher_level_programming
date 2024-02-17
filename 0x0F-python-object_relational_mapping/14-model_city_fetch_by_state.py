7c383bcb17c0bbbfac7200d03df5f7dd6d995531
nadashaban10 <nadashaban10@github.com>
2024-02-17 14:40:11 +0200

    ORMSQL

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,7 +10,6 @@ from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# should not be executed when imported
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),

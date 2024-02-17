614ab519af73d181ba52bd813dbf96e3f1dbfa3b
nadashaban10 <nadashaban10@github.com>
2024-02-17 14:41:25 +0200

    ORMSQL

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-metadata = MetaData()
 
 
 class City(Base):

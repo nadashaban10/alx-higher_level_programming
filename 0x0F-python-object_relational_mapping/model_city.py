d7918cbfd45dca7102ce44d161d3538099c853f6
nadashaban10 <nadashaban10@github.com>
2024-02-17 14:37:53 +0200

    ORMSQL

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """a python file that contains the class definition of a cities"""
 
-from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy import ForeignKey, MetaData, Column, Integer, String
 from model_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
 
-
+Base = declarative_base()
+metadata = MetaData()
 class City(Base):
     """State class"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    

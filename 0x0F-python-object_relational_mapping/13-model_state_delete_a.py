626adfb94c990b00e0570589ab31ef8b1179be8c
nadashaban10 <nadashaban10@github.com>
2024-02-17 03:08:05 +0200

    ORMSQL

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     session = Session()
 
     states = session.query(State).filter(
-        State.name.like('%a%')).all()
+        State.name.like('%a%')).order_by(State.id).all()
     for state in states:
         session.delete(state)
 session.commit()

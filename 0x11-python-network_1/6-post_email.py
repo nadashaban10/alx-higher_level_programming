16aa6e05cd950da4ab7f2ea620812752fbf0a8ca
nadashaban10 <nadashaban10@github.com>
2024-03-01 18:41:33 +0200

    task6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that fetches url given"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)

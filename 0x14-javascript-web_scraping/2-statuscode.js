af7177b6b97826f3790f9fcbb7a409117a745783
nadashaban10 <nadashaban10@github.com>
2024-03-27 02:55:50 +0200

    task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 
-request = (process.argv[2], (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-    } else {
-        console.log('code:' (response.statusCode));
-    }
-    });
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log('code: ${response.statusCode}');
+  }
+});

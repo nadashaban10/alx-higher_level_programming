e90554150924f848c79fe6e3fd08923f16df919d
nadashaban10 <nadashaban10@github.com>
2024-03-27 02:47:40 +0200

    task2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
   if (err) throw err;
 });

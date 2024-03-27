566606d7d40b9988c7ecdbd6785275a5e4e9a97d
nadashaban10 <nadashaban10@github.com>
2024-03-27 04:10:23 +0200

    task6

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = 18;
+const URL = process.argv[2];
 
-request(apiUrl, (error, response, body) => {
+request(URL, (error, response, body) => {
   if (error) {
-    console.error('An error occurred:', error);
-    return;
+    console.error('error:', error); // Print the error if one occurred
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
-
-  const films = JSON.parse(body).results;
-  const filmsWithWedge = films.filter(film => film.characters.includes(characterId));
-
-  console.log(filmsWithWedge.length);
 });
